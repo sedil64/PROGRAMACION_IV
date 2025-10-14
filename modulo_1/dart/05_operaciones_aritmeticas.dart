3c007378cbbb644e2b6777a7919bb065c6c2ab70
sedil64 <dilan6465@gmail.com>
2025-10-13 21:55:20 -0500

    Clase del 13/10/2025

---
+++ modulo_1/dart/05_operaciones_aritmeticas.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Operaciones aritmeticas");
+  final double number1 = 12.5;
+  final double number2 = 5;
+
+  print("Suma $number1+$number2 = ${number1 + number2}");
+  print("Resta $number1-$number2 = ${number1 - number2}");
+  print("Multiplicacion $number1*$number2 = ${number1 * number2}");
+  print("Division $number1/$number2 = ${number1 / number2}");
+}
